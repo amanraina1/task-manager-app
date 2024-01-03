bae6451d54cb2f239d6f52a4eb08dff922319cac
amanraina1 <amanraina9086@gmail.com>
2024-01-03 17:25:14 +0530

    connected db succesfully

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 const express = require("express");
+const connectDB = require("./db/connect");
+const tasks = require("./routes/tasks");
 require("dotenv").config();
 
 const app = express();
 
+//middleware
+app.use(express.json());
+
+// routes
 app.get("/", (req, res) => {
   res.send("Hello Aman");
 });
 
-app.listen(process.env.PORT, (error) => {
-  console.log(`Server is listening on PORT : ${process.env.PORT}...`);
-});
+app.use("/api/v1/tasks", tasks);
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGODB_URI);
+    app.listen(process.env.PORT, (error) => {
+      console.log(`Server is listening on PORT : ${process.env.PORT}...`);
+    });
+  } catch (error) {
+    console.log("Error", error);
+  }
+};
+start();
+// app.get("/api/v1/tasks");  - get all tasks
+// app.post("/api/v1/tasks");  - create a new task
+// app.get("/api/v1/tasks/:id");  - get a single task
+// app.patch("/api/v1/tasks/:id");  - update a single task
+// app.delete("/api/v1/tasks/:id");  - delete task
---
+++ controllers/tasks.js
@@ -0,0 +1,29 @@
+const Task = require("../models/tasks");
+
+const getAllTasks = (req, res) => {
+  res.send("get all tasks from the file");
+};
+
+const createTask = (req, res) => {
+  res.json(req.body);
+};
+
+const getTask = (req, res) => {
+  res.json({ id: req.params.id });
+};
+
+const updateTask = (req, res) => {
+  res.json({ id: req.params.id });
+};
+
+const deleteTask = (req, res) => {
+  res.json({ id: req.params.id });
+};
+
+module.exports = {
+  getAllTasks,
+  createTask,
+  updateTask,
+  deleteTask,
+  getTask,
+};
