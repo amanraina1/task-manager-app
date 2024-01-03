3f0f6ebfe1f5295f0419aebffa4990d1ebf383ff
amanraina1 <amanraina9086@gmail.com>
2024-01-04 03:24:58 +0530

    created the post and create route in frontend

--- public/index.js
+++ public/index.js
@@ -1,4 +1,28 @@
 const bottomBox = document.querySelector("#bottom-box");
+const btn = document.querySelector("#button");
+const inputVal = document.getElementById("input");
+var app;
+
+const inputText = input.addEventListener(
+  "keyup",
+  (e) => (app = e.target.value)
+);
+
+btn.addEventListener("click", async () => {
+  await fetch("http://localhost:3000/api/v1/tasks", {
+    method: "POST",
+    body: JSON.stringify({
+      name: app,
+      completed: false,
+    }),
+    headers: {
+      "Content-type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+  window.location.reload();
+});
 
 const data = async () => {
   const apiData = await fetch("http://localhost:3000/api/v1/tasks");
