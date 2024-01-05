b6530549cd4dc6fdc2ccc30faf18c870fb900b4b
amanraina1 <amanraina9086@gmail.com>
2024-01-05 17:36:58 +0530

    implemented delete request and implemented checkbox based on user input

--- public/index.js
+++ public/index.js
@@ -2,6 +2,19 @@ const bottomBox = document.querySelector("#bottom-box");
 const btn = document.querySelector("#button");
 const inputVal = document.getElementById("input");
 
+// DELETE A TASK
+async function fE(e) {
+  await fetch(`http://localhost:3000/api/v1/tasks/${e.target.id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+  window.location.reload();
+}
+
 //CREATE NEW TASK REQ
 var app;
 const inputText = input.addEventListener(
@@ -35,23 +48,23 @@ const data = async () => {
     const ul = document.createElement("section");
     if (task.completed) {
       ul.innerHTML = `
-        <span class="icons-left">
-              <i class="fa-regular fa-circle-check check"></i>
-              <span class="text line" id="${task._id}">${task.name}</span>
-            </span>
-            <span class="icons-right">
-            <a href="task.html"><i class="fa-solid fa-pen-to-square edit"></i></a>
-              <i class="fa-solid fa-trash delete" style="color: #c22424"></i>
-            </span>
-        `;
+      <span class="icons-left">
+      <i class="fa-regular fa-circle-check check"></i>
+      <span class="text line">${task.name}</span>
+      </span>
+      <span class="icons-right">
+      <a href=task.html><i id="${task._id}" onclick="getData(event)" class="fa-solid fa-pen-to-square edit"></i></a>
+      <i class="fa-solid fa-trash delete" onclick="fE(event)" id="${task._id}"></i>
+      </span>
+      `;
     } else {
       ul.innerHTML = `
           <span class="icons-left">
-                <span class="text" id="${task._id}">${task.name}</span>
-              </span>
+            <span class="text">${task.name}</span>
+          </span>
               <span class="icons-right">
-              <a href="task.html"><i class="fa-solid fa-pen-to-square edit"></i></a>
-                <i class="fa-solid fa-trash delete" style="color: #c22424"></i>
+                <a href=task.html><i id="${task._id}" onclick="getData(event)" class="fa-solid fa-pen-to-square edit"></i></a>
+                <i class="fa-solid fa-trash delete" onclick="fE(event)" id="${task._id}"></i>
               </span>
           `;
     }
@@ -60,3 +73,8 @@ const data = async () => {
   });
 };
 data();
+
+// GET A SINGLE TASK
+function getData(event) {
+  window.localStorage.setItem("id", event.target.id);
+}
--- public/task.js
+++ public/task.js
@@ -1,1 +1,47 @@
+// GET A SINGLE TASK
 
+var id = window.localStorage.getItem("id");
+async function patch() {
+  const dataApi = await fetch(`http://localhost:3000/api/v1/tasks/${id}`);
+  const json = await dataApi.json();
+  const checked = json.completed;
+
+  if (checked) {
+    check();
+  } else {
+    unCheck();
+  }
+  upadte(json);
+}
+async function upadte(json) {
+  const p = document.getElementsByClassName("task-para");
+  const input = document.getElementsByClassName("write");
+
+  p[0].innerText = json._id;
+  input[0].setAttribute("value", json.name);
+}
+document.addEventListener("DOMContentLoaded", patch);
+
+//default checking for completed checkbox
+async function check() {
+  document.querySelector(".check-box").checked = true;
+}
+async function unCheck() {
+  document.querySelector(".check-box").checked = false;
+}
+
+// CHANGING THE CHECKBOX BASED ON THE INPUT
+const checkBox = document.querySelector(".check-box");
+checkBox.addEventListener("click", async () => {
+  await fetch(`http://localhost:3000/api/v1/tasks/${id}`, {
+    method: "PATCH",
+    body: JSON.stringify({
+      completed: checkBox.checked,
+    }),
+    headers: {
+      "Content-type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+});
